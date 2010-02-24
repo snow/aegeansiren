b41dafb2b28c741b1cc1c1ef0b019231ecfce1a1
snow <snow.hellsing@gmail.com>
2010-02-24 06:02:43 +0000

    add S::isEmail($string)
    git-svn-id: https://fb-aegeansiren.googlecode.com/svn/trunk@43 2aa56b66-87e7-11de-9934-f1fc26e2a4cc

--- zii/S.php
+++ zii/S.php
@@ -34,4 +34,11 @@ class S
             return substr($word,0,-1);
         }
     }
+
+    public static function isEmail($string)
+    {
+    	$pattern='/^[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+\\/=?^_`{|}~-]+)*'
+    	    .'@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$/';
+    	return preg_match($pattern,$string);
+    }
 }
