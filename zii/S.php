a79e6c35ee9aea4c35507f2567f600a4d6bce39a
snow <snow.hellsing@gmail.com>
2010-06-24 09:01:40 +0000

    git-svn-id: https://fb-aegeansiren.googlecode.com/svn/trunk@71 2aa56b66-87e7-11de-9934-f1fc26e2a4cc

--- zii/S.php
+++ zii/S.php
@@ -46,4 +46,50 @@ class S
 	{
 		return is_string($url)?current(explode('/',array_pop(explode('//',$url,2)),2)):'';
 	}
+
+	public static function getRandomString($type = 'alnum', $len = 8)
+	{
+		switch($type)
+		{
+			case 'basic'	: return mt_rand();
+			  break;
+			case 'alnum'	:
+			case 'numeric'	:
+			case 'nozero'	:
+			case 'alpha'	:
+
+					switch ($type)
+					{
+						case 'alpha'	:	$pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+							break;
+						case 'alnum'	:	$pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+							break;
+						case 'numeric'	:	$pool = '0123456789';
+							break;
+						case 'nozero'	:	$pool = '123456789';
+							break;
+					}
+
+					$str = '';
+					for ($i=0; $i < $len; $i++)
+					{
+						$str .= substr($pool, mt_rand(0, strlen($pool) -1), 1);
+					}
+					return $str;
+			  break;
+			case 'unique'	:
+			case 'md5' 		:
+
+						return md5(uniqid(mt_rand()));
+			  break;
+			case 'encrypt'	:
+			case 'sha1'	:
+
+						$CI =& get_instance();
+						$CI->load->helper('security');
+
+						return do_hash(uniqid(mt_rand(), TRUE), 'sha1');
+			  break;
+		}
+	}
 }
