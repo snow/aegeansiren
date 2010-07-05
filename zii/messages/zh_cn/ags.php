332090a99a5c69d84fffb351719b42870180e74c
snow <snow.hellsing@gmail.com>
2010-07-05 11:08:35 +0000

    git-svn-id: https://fb-aegeansiren.googlecode.com/svn/trunk@91 2aa56b66-87e7-11de-9934-f1fc26e2a4cc

--- zii/messages/zh_cn/ags.php
+++ zii/messages/zh_cn/ags.php
@@ -83,6 +83,7 @@ return array(
 	'err:accountNotActivated'=>'帐号还未激活。',
 	'err:wrongPassword'=>'密码不正确',
 	'err:invalidParams'=>'提供了非法的参数 {where}:{paramInfo}',
+	'err:invalidRequest'=>'U\'re done here,silly rabbit',
 	'err:internal'=>'/. .\ 遇到了一些小麻烦，再试一次看看有没有好运气？',
 	'err:mkdirFail'=>'创建目录失败',
 	/**
