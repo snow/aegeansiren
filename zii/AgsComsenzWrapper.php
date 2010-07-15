c34874533961320bb26783cb2118a639ba6ea92b
snow <snow.hellsing@gmail.com>
2010-07-15 10:01:10 +0000

    git-svn-id: https://fb-aegeansiren.googlecode.com/svn/trunk@104 2aa56b66-87e7-11de-9934-f1fc26e2a4cc

--- zii/AgsComsenzWrapper.php
+++ zii/AgsComsenzWrapper.php
@@ -3,49 +3,66 @@
 class AgsComsenzWrapper extends CComponent
 {
 	public $comsenzClientPath;
-	public $connect = '';
 	public $key;
 	public $apiUrl;
 	public $apiIp;
-	public $charset = 'utf8';
 	public $appId;
-	
+	public $ppp;
+	public $charset = 'utf8';
+
+	public $connect;
 	public $dbHost;
 	public $dbUser;
 	public $dbPassword;
 	public $dbName;
 	public $dbCharset = 'utf8';
 	public $dbTablePre = '';
-	public $dbDbConnect = true;
-	
+	public $dbConnect = true;
+
 	public function init()
 	{
-		if (!($this->comsenzClientPath
-			&& $this->key
+		if (!($this->key
 			&& $this->apiUrl
 			&& $this->apiIp
 			&& $this->appId
 			&& ($this->connect?
 				($this->dbHost && $this->dbUser && $this->dbPassword && $this->dbName):true)))
 		{
-			throw new CException('err:needed');
+			throw new CException('err:missingRequiredParams');
 		}
-		
-		require_once Yii::getPathOfAlias('application.comsenzClient').'/client.php';
+
+		define('UC_KEY', $this->key);
+		define('UC_API', $this->apiUrl);
+		define('UC_IP', $this->apiIp);
+		define('UC_APPID', $this->appId);
+		define('UC_PPP', $this->ppp);
+		define('UC_CHARSET', $this->charset);
+
+		define('UC_CONNECT', $this->connect);
+		define('UC_DBHOST', $this->dbHost);
+		define('UC_DBUSER', $this->dbUser);
+		define('UC_DBPW', $this->dbPassword);
+		define('UC_DBNAME', $this->dbName);
+		define('UC_DBTABLEPRE', $this->dbTablePre);
+		define('UC_DBCHARSET', $this->dbCharset);
+		define('UC_DBCONNECT', $this->dbConnect);
 	}
-	
+
 	public function syncLogin($uid)
 	{
+		require_once Yii::getPathOfAlias('application.comsenzClient').'/client.php';
 		return uc_user_synlogin($uid);
 	}
-	
+
 	public function syncLogout()
 	{
+		require_once Yii::getPathOfAlias('application.comsenzClient').'/client.php';
 		return uc_user_synlogout();
 	}
-	
+
 	public function getUser($username,$isUid=false)
 	{
+		require_once Yii::getPathOfAlias('application.comsenzClient').'/client.php';
 		return uc_get_user($username, $isUid);
 	}
 }
