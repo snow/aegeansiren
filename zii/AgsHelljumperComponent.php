28f0083608fc2cf680713c4e298ae81c71b904fa
snow <snow.hellsing@gmail.com>
2010-08-11 08:26:25 +0000

    git-svn-id: https://fb-aegeansiren.googlecode.com/svn/trunk@140 2aa56b66-87e7-11de-9934-f1fc26e2a4cc

---
+++ zii/AgsHelljumperComponent.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * this class make its subclass able to import into other apps
+ * useage:
+ * 'components'=>array(
+ * 	'classId'=>array(
+ * 		'class'=>'alias.to.your.subclass',
+ * 		'dataAccessMode'=>'db', //or soap
+ * 	),
+ * ),
+ *
+ * @author snow
+ *
+ */
+abstract class AgsHelljumperComponent extends CComponent
+{
+	const DATA_ACCESS_NATIVE = 'native';
+	const DATA_ACCESS_DB = 'db';
+	const DATA_ACCESS_SOAP = 'soap';
+
+	protected $config;
+
+	/**
+	 *
+	 * @var string enum('native','db','soap')
+	 */
+	public $dataAccessMode;
+	/**
+	 *
+	 * @var string
+	 */
+	public $remoteApiUri;
+
+	protected $_dbConnection;
+
+	/**
+	 * @return array database config for helljumper to instantiate a CDbConnection instance
+	 */
+	abstract public function getConfigFilePath();
+
+	/**
+	 * plz do call parent::init() in child classes
+	 */
+	public function init()
+	{
+		if (file_exists($this->configFilePath))
+		{
+			$this->config = include($this->configFilePath);
+		}
+		else
+		{
+			throw new CException('err:missingConfig:'.get_class($this));
+		}
+
+		// ensure data access
+		switch ($this->dataAccessMode)
+		{
+			case self::DATA_ACCESS_DB:
+				if (isset($this->config['db']) && is_array($this->config['db']))
+				{
+					$this->_dbConnection = new CDbConnection;
+
+					foreach ($this->dbConfig as $key=>$value)
+					{
+						$this->_dbConnection->$key = $value;
+					}
+
+					$this->_dbConnection->active = true;
+				}
+				else
+				{
+					throw new CException('err:invalidDbConfig:'.get_class($this));
+				}
+			break;
+
+			case self::DATA_ACCESS_NATIVE:
+				$this->_dbConnection = Y::a()->db;
+			break;
+
+			default:
+				throw new CException('err:incomplete');
+			break;
+		}
+	}
+}
