c97bf82a822c633e47005e353c8d669e9c24367d
snow <snow.hellsing@gmail.com>
2010-10-11 03:30:55 +0000

    git-svn-id: https://fb-aegeansiren.googlecode.com/svn/trunk@190 2aa56b66-87e7-11de-9934-f1fc26e2a4cc

--- zii/AgsAR.php
+++ zii/AgsAR.php
@@ -1,12 +1,17 @@
 <?php
 /**
  * AegeanSiren ActiveRecord
+ * a set of enhancement of ActiveRecord
+ * ActiveRecord的一系列增强
  *
  * New: metadata support
  * New: auto timestamp
  * Chg: prefer to getter or setter method than to access property directly in __get() and __set()
  *
- * @author snow.hellsing@gmail.com
+ * 增加metadata支持
+ * 自动对created和updated字段加timestamp
+ *
+ * @author snow@firebloom.cc
  *
  */
 abstract class AgsAR extends CActiveRecord
@@ -18,6 +23,7 @@ abstract class AgsAR extends CActiveRecord
 	{
 		parent::init();
 
+		// init internal metadata array if the meta column do exist
 		if ($this->hasAttribute($this->getAgsMetaColumn()))
 		{
 			$this->_agsMetadata = $this->getAgsMetaDefaults();
@@ -26,6 +32,10 @@ abstract class AgsAR extends CActiveRecord
 			{
 				$this->_agsMetadata = array();
 			}
+			// Q: why not if...else...?
+			// A: to avoid call $this->getAgsMetaDefaults() twice
+			// Q: 为什么不 if...else...?
+			// A: 为了避免两次调用 $this->getAgsMetaDefaults()
 		}
 	}
 
@@ -33,17 +43,28 @@ abstract class AgsAR extends CActiveRecord
 	{
 		parent::afterFind();
 
+		// load metadata from database
 		if (is_array($record = json_decode($this->getAttribute($this->getAgsMetaColumn()),true)))
 		{
 			$this->_agsMetadata = array_merge($this->_agsMetadata,$record);
 		}
 	}
 
+	/**
+	 * subclass could override this method returnning a different column to store matadata
+	 * 子类可以通过重载这个方法，返回其它行来提供metadata
+	 */
 	protected function getAgsMetaColumn()
 	{
 		return 'metaSerial';
 	}
 
+	/**
+	 * override to make getter method has priority and check metadata
+	 * 重载以使得getter方法有最高优先级并且会检查metadata
+	 *
+	 * @param string $name
+	 */
 	public function __get($name)
 	{
 		$getter = 'get'.$name;
@@ -62,6 +83,13 @@ abstract class AgsAR extends CActiveRecord
 		}
 	}
 
+	/**
+	 * override to make setter method has priority and check metadata
+	 * 重载以使得setter方法有最高优先级并且会检查metadata
+	 *
+	 * @param string $name
+	 * @param mixed $value
+	 */
 	public function __set($name,$value)
 	{
 		$setter = 'set'.$name;
@@ -70,6 +98,10 @@ abstract class AgsAR extends CActiveRecord
 		{
 			$this->$setter($value);
 		}
+		// only use magic set on existed metadata
+		// call setAgsMetadata() to manually add metadata
+		// 只通过魔术方法给已经存在的metadata赋值
+		// 调用实例的 setAgsMetadata() 方法来手动添加新的metadata
 		elseif ($this->hasAgsMetadata($name))
 		{
 			$this->setAgsMetadata($name,$value);
@@ -77,6 +109,37 @@ abstract class AgsAR extends CActiveRecord
 		else
 		{
 			parent::__set($name,$value);
+		}
+	}
+
+	/**
+	 * subclass could override this method
+	 * returning a key=>value array as meta defaults
+	 * and $this->hasAgsMetadata($key) will depend on if a key exists in this array
+	 *
+	 * 子类可重载此方法
+	 * 返回一个关联数组用作metadata默认值
+	 * 并且这个数据的键将作为$this->hasAgsMetadata($key)的判断依据
+	 */
+	public function getAgsMetaDefaults()
+	{
+		return self::AGS_METADATA_KEY_MODE_AUTO;
+	}
+
+	/**
+	 *
+	 * @param string $key
+	 */
+	public function hasAgsMetadata($key)
+	{
+		// on auto mode,check current internal metadata array
+		if (self::AGS_METADATA_KEY_MODE_AUTO === $this->getAgsMetaDefaults())
+		{
+			return (is_array($this->_agsMetadata) && key_exists($key,$this->_agsMetadata));
+		}
+		else
+		{
+			return key_exists($key,$this->getAgsMetaDefaults());
 		}
 	}
 
@@ -112,23 +175,11 @@ abstract class AgsAR extends CActiveRecord
 		}
 	}
 
-	public function hasAgsMetadata($key)
-	{
-		if (self::AGS_METADATA_KEY_MODE_AUTO === $this->getAgsMetaDefaults())
-		{
-			return (is_array($this->_agsMetadata) && key_exists($key,$this->_agsMetadata));
-		}
-		else
-		{
-			return key_exists($key,$this->getAgsMetaDefaults());
-		}
-	}
-
-	public function getAgsMetaDefaults()
-	{
-		return self::AGS_METADATA_KEY_MODE_AUTO;
-	}
-
+	/**
+	 * override to provide attr label by language system
+	 *
+	 * @param string $attribute
+	 */
 	public function getAttributeLabel($attribute)
 	{
 		$messageKey = get_class($this).':'.$attribute.':label';
@@ -148,6 +199,7 @@ abstract class AgsAR extends CActiveRecord
 
 	protected function beforeValidate()
 	{
+		// auto timestamp
 		if (in_array($this->scenario,array('insert','update')))
 		{
 			$time = time();
